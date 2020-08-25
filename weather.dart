4b0249a1240ef033cb27e22d32a01fb6ff296dc9
shi-xiaoli <1551329512@qq.com>
2020-08-25 12:57:12 +0800

    weather v2.0

--- weather.dart
+++ weather.dart
@@ -3,99 +3,126 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+const key = 'a17d10bd349e432d99228b4331ed7628';
 //key = a17d10bd349e432d99228b4331ed7628
 //实况天气  https://devapi.heweather.net/v7/weather/now?{请求参数}
 //城市信息  https://geoapi.heweather.net/v2/city/lookup?{请求参数}
+//3天预报 https://devapi.heweather.net/v7/weather/3d?{请求参数}
 //南京 101190101
 class WeatherPage extends StatefulWidget {
   @override
-  WeatherPageState createState() => WeatherPageState();
+  WeatherPageState pstate = WeatherPageState();
+  WeatherPageState createState() => pstate;
 }
 
 class WeatherPageState extends State<WeatherPage> {
   //data
   WeatherData weather = WeatherData.empty();
-  
-  WeatherState() {
-    _getWeather();
-  }
+  String lat = '39.1';
+  String lon = '116.4';
+  String cityname = '';
+  String cityid = '';
+  List<String> _chinacitys = [
+        "北京市 101010100",
+        "重庆市 101040100",
+        "上海市 101020100",
+        "天津市 101030100",
+        "香港特别行政区 101320101"
+        "澳门特别行政区 101330101",
+        "哈尔滨市 101050101",
+        "阿城市 101050104",
+        "双城市 101050102",
+        "尚志市 101050111",
+        "五常市 101050112",
+        "齐齐哈尔市 101050201",
+        "讷河市 101050202",
+        "鸡西市 101051101",
+        "虎林市 101051102",
+        "密山市 101051103"
 
-   _getWeather() async{
+
+
+  ];
+
+  _getWeather() async{
     WeatherData data = await _fetchWeather();
     setState((){
       weather = data;
     });
   }
 
-  Future<WeatherData>_fetchWeather() async{
-    print("请求网络-----");
-    final response = await http.get('https://devapi.heweather.net/v7/weather/now?location=101010100&key=a17d10bd349e432d99228b4331ed7628');
-    //final response = await http.get('https://free-api.heweather.com/s6/weather/now?location=广州&key=ebb698e9bb6844199e6fd23cbb9a77c5');
-    //if(response.statusCode == 200){
-    if(response.statusCode == 200){
-      print("请求成功");
-      print(response.body);
+  Future<WeatherData>_fetchWeather() async {
+    final response = await http.get(
+        'https://devapi.heweather.net/v7/weather/now?location=${cityid}&key=${key}');
+    if (response.statusCode == 200) {
       return WeatherData.fromJson(json.decode(response.body));
-    }else{
-      print("请求出错，返回状态码：");
+    } else {
       print(response.statusCode);
       return new WeatherData();
     };
   }
 
-  Widget build(BuildContext context){
-    return Container(
-      child: Column(
-        children: <Widget>[
-          new Container(
-            width: double.infinity,
-            margin: EdgeInsets.only(top: 40.0),
-            child: new Text(
-              "南京",
-              textAlign: TextAlign.center,
-              style: new TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-              ),
-            ),
-          ),
-          new Container(
-            width: double.infinity,
-            margin: EdgeInsets.only(top: 100.0),
-            child: new Column(
-              children: <Widget>[
-                new Text(
-                    weather?.tmp,
-                    style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 80.0
-                    )),
-                new Text(
-                    weather?.cond,
-                    style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 45.0
-                    )),
-                new Text(
-                  weather?.hum,
-                  style: new TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0
-                  ),
-                ),
-                new IconButton(
-                  color: Colors.lightBlue,
-                  icon: Icon(Icons.add_location),
-                  onPressed: _getWeather,
-                ),
-              ],
-            ),
-          )
-        ]
-      )
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        endDrawer: Drawer(
+          child: buildCityList(),
+        ),
+        body: new Column(
+            children: <Widget>[
+                new Container(
+                   width: double.infinity,
+                   margin: EdgeInsets.only(top: 100.0),
+                    child: new Column(
+                       children: <Widget>[
+                         new Text(
+                           cityname,
+                           textAlign: TextAlign.center,
+                            style: new TextStyle(color: Colors.white, fontSize: 30.0,),
+                          ),
+                         new Text(
+                            weather?.tmp,
+                            style: new TextStyle(color: Colors.white, fontSize: 30.0)
+                           ),
+                          new Text(
+                                weather?.cond,
+                                style: new TextStyle(color: Colors.white, fontSize: 30.0)
+                          ),
+                          new Text(
+                                 weather?.hum,
+                                 style: new TextStyle(color: Colors.white, fontSize: 30.0),
+                              ),
 
+                       ],
+                    ),
+                ),
+           ]
+        )
     );
   }
+
+  Widget buildCityList() {
+    return new ListView.builder(
+      itemCount: _chinacitys.length,
+      itemBuilder: (context, i) {
+        return _buildRow(_chinacitys[i].split(" ")[0], i);
+      },
+    );
+  }
+
+  Widget _buildRow(String city, int i) {
+    return new ListTile(
+      title: new Text(
+        city,
+        style: TextStyle(color: Colors.white, fontSize: 20.0),
+      ),
+      onTap: () {
+        cityname = _chinacitys[i].split(" ")[0];
+        cityid = _chinacitys[i].split(" ")[1];
+        _getWeather();
+      },
+    );
+  }
+
 }
 
 class WeatherData{
@@ -107,11 +134,6 @@ class WeatherData{
 
   factory WeatherData.fromJson(Map<String, dynamic> json) {
     return WeatherData(
-      /*
-      cond: json['HeWeather7'][0]['now']['text'],
-      tmp: json['HeWeather7'][0]['now']['temp']+"°",
-      hum: "湿度  "+json['HeWeather7'][0]['now']['humidity']+"%",
-       */
       cond: json['now']['text'],
       tmp: json['now']['temp']+"°",
       hum: "湿度  "+json['now']['humidity']+"%",
@@ -125,6 +147,4 @@ class WeatherData{
       hum: "",
     );
   }
-
 }
-
